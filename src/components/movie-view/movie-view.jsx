178a3c8d11e0d1bb03a538ed54740af01fb111c0
jbettmann <jordan.bettmann@gmail.com>
2022-03-03 21:18:50 -0700

    added more details

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 
 export class MovieView extends React.Component {
+
   render() {
     // movieData is "name of the prop" used in <MovieCard ... />
     const { movieData, onBackClick } = this.props;
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movieData.ImagePath} />
+          <img src={movieData.ImageUrl} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
@@ -18,19 +19,28 @@ export class MovieView extends React.Component {
           <span className="label">Description: </span>
           <span className="value">{movieData.Description}</span>
         </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movieData.Genre.Name}</span>
+        </div>
         <div className="movie-director">
           <span className="label">Director: </span>
-          <span className="value">{movieData.Director}</span>
-        </div>
-        <div className="movie-actors">
-          <span className="label">Actors: </span>
-          <span className="value">{movieData.Actors}</span>
+          <span className="value">{movieData.Director.Name}</span>
         </div>
         <div className="movie-releaseDate">
           <span className="label">Release Date: </span>
           <span className="value">{movieData.Release}</span>
         </div>
+        <div className="movie-actors">
+          <span className="label">Actors: </span>
+          <span className="value">{movieData.Actors}</span>
+        </div>
+        <div className="movie-featured">
+          <span className="label">Featured: </span>
+          <span className="value">{movieData.Featured}</span>
+        </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>
-    )}
+    )};
+
 }
