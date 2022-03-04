a7b4bfaeb58c7fcaa53828bfd919ff1049b794e3
jbettmann <jordan.bettmann@gmail.com>
2022-03-03 22:32:14 -0700

    added sign up button

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -17,16 +17,19 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <div>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
+      <button>Sign Up</button>  
+     </div>
   );
 }
