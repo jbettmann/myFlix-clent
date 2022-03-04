f60d5ea3e06f2fff1e5b36878b35d33a0b00c485
jbettmann <jordan.bettmann@gmail.com>
2022-03-03 21:19:12 -0700

    added propTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
@@ -8,3 +9,24 @@ export class MovieCard extends React.Component {
     return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
   }
 }
+
+MovieCard.propTypes = {
+  // props object MUST include a movie object. .shape({...}) means object
+  movieData: PropTypes.shape({
+    // props object MAY contain a title key, if it DOES then it MUST be a string
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    ImageUrl: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Release: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
+    Actors: PropTypes.array.isRequired
+  }).isRequired,
+  // props object MUST contain onMovieClick and MUST be a function 
+  onMovieClick: PropTypes.func.isRequired
+};
