f31e6e3eb66adea23b40f29bd909688131860494
jbettmann <jordan.bettmann@gmail.com>
2022-03-16 21:27:13 -0600

    added director view and styling

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import {Container, Card, Button, Row} from 'react-bootstrap';
 
 export class DirectorView extends React.Component {
@@ -8,45 +7,49 @@ export class DirectorView extends React.Component {
         const { director, onBackClick, movies } = this.props;
 
         return (
-            <Container fluid>
-                <Card>
-                    <Card.Body>
-                        <Card.Title>Director</Card.Title>
-                        <Card.Text>
-                            <span className="label">Name: </span>
-                            <span className="value">{director.Name}</span>
-                        </Card.Text>
-                        <Card.Text>
-                            <span className="label">Bio: </span>
-                            <span className="value">{director.Bio}</span>
-                        </Card.Text>
-                        <Card.Text>
-                            <span className="label">Birth: </span>
-                            <span className="value">{director.Birth}</span>
-                        </Card.Text>
-                        <Card.Text>
-                            <span className="label">Death: </span>
-                            <span className="value">{director.Death}</span>
-                        </Card.Text>
+            <Container >
+              <Card id='movie-card'>
+                  <Card.Body>
+                      <Card.Title></Card.Title>
+                      <Card.Text>
+                          <span className="label">Name: </span>
+                          <span className="value">{director.Name}</span>
+                      </Card.Text>
+                      <Card.Text>
+                          <span className="label">Bio: </span>
+                          <span className="value">{director.Bio}</span>
+                      </Card.Text>
+                      <Card.Text>
+                          <span className="label">Birth: </span>
+                          <span className="value">{director.Birth}</span>
+                      </Card.Text>
+                      <Card.Text>
+                          <span className="label">Death: </span>
+                          <span className="value">{director.Death}</span>
+                      </Card.Text>
 
-                        <Button variant="outline-light" onClick={() => { onBackClick(); }}>Back</Button>
-                    </Card.Body>
-                </Card>
-                <Row>
+                      <Button variant="light" onClick={() => { onBackClick(); }}>Back</Button>
+                  </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body>
+                  <Row>
                     {movies.map(movie => (
-                        <Card className="favorite-movie card-content" key={movie._id} >
-                            <Card.Img
-                                className="fav-poster"
-                                variant="top"
-                                src={movie.ImagePath} />
-                            <Card.Body style={{ backgroundColor: "black" }}>
-                                <Card.Title className="movie_title">
-                                    {movie.Title}
-                                </Card.Title>
-                            </Card.Body>
-                        </Card>
+                      <Card className="favorite-movie card-content" key={movie._id} >
+                        <Card.Img
+                          className="fav-poster"
+                          variant="top"
+                          src={movie.ImageUrl} />
+                        <Card.Body>
+                          <Card.Title className="movie_title">
+                            {movie.Title}
+                          </Card.Title>
+                        </Card.Body>
+                      </Card>
                     ))}
-                </Row>
+                  </Row>
+                </Card.Body>
+              </Card>
             </Container>
         );
     }
