5c7a694328d35608c57a33567803dd53bd9ee744
jbettmann <jordan.bettmann@gmail.com>
2022-03-16 21:28:41 -0600

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,60 +3,55 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 import './movie-view.scss';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
-    keypressCallback(event) {
-        console.log(event.key);
-    }
-
-    componentDidMount() {
-        document.addEventListener('keypress', this.keypressCallback);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('keypress', this.keypressCallback);
-    }
-
     render() {
+        // movieData is "name of the prop" used in <MovieCard ... />
         const { movie, onBackClick } = this.props;
-
         return (
-            <Container fluid className="moviesContainer">
-                <Row>
-                    <Col>
-                        <div className="movie-view">
-                            <div className="movie-poster">
-                                <img src={movie.ImagePath}/>
-                            </div>
-                            <div className="movie-title">
-                                <span className="label">Title: </span>
-                                <span className="value">{movie.Title}</span>
-                            </div>
-                            <div className="movie-description">
-                                <span className="label">Description: </span>
-                                <span className="value">{movie.Description}</span>
-                            </div>
-                            <div className="movie-genre">
-                                <span className="label">Genre: </span>
-                                <Link to={`/genres/${movie.Genre.Name}`}>
-                                    <Button variant="link">{movie.Genre.Name}</Button>
-                                </Link>
-                            </div>
-                            <div className="movie-director">
-                                <span className="label">Director: </span>
-                                <Link to={`/directors/${movie.Director.Name}`}>
-                                    <Button variant="link">{movie.Director.Name}</Button>
-                                </Link>
-                            </div>
-
-                            <Button variant="outline-light" onClick={() => onBackClick(null)}>Back</Button>
-
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        );
+            <Card id="movie-card" variant="dark">
+                <Card.Body>
+                    <div className="movie-poster">
+                      <img src={movie.ImageUrl} />
+                    </div>
+                    <div className="movie-title">
+                      <span className="label">Title: </span>
+                      <span className="value">{movie.Title}</span>
+                    </div>
+                    <div className="movie-description">
+                      <span className="label">Description: </span>
+                      <span className="value">{movie.Description}</span>
+                    </div>
+                    <div className="movie-genre">
+                      <span className="label">Genre: </span>
+                    <Link to={`/genres/${movie.Genre.Name}`}>
+                        <Button id="noLinkLook" variant="link">{movie.Genre.Name}</Button>
+                    </Link>
+                    </div>
+                    <div className="movie-director">
+                      <span className="label">Director: </span>
+                    <Link  to={`/directors/${movie.Director.Name}`}>
+                        <Button id="noLinkLook" variant="link">{movie.Director.Name}</Button>
+                    </Link>
+                    </div>
+                    <div className="movie-releaseDate">
+                      <span className="label">Release Date: </span>
+                      <span className="value">{movie.Release}</span>
+                    </div>
+                    <div className="movie-actors">
+                      <span className="label">Actors: </span>
+                      <span className="value">{movie.Actors}</span>
+                    </div>
+                    <div className="movie-featured">
+                      <span className="label">Featured: </span>
+                      <span className="value">{movie.Featured}</span>
+                    </div>
+                    {/* button sets selectedMovie to null, allowing MainView to stop rendering MovieView */}
+                    <Button variant="light" onClick={() => { onBackClick(); }}>Back</Button>
+                </Card.Body>
+            </Card>
+        )
     }
 }
