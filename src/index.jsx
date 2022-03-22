57e15710d04d421d0a69c1706c7692c94f3f47ee
jbettmann <jordan.bettmann@gmail.com>
2022-03-21 20:12:25 -0600

    Update index.js to include Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view'; // If MainView doesnt have "default" keyword in main-view.jsx, you must surround it in {} here.
+import { Container } from 'react-bootstrap';
+import { createStore } from 'redux'; // fourth and fifth line here import "connect()" fucntion and React Redux. 
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import MainView from './components/main-view/main-view'; // If MainView doesnt have "default" keyword in main-view.jsx, you must surround it in {} here.
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+// makes app entirely accessable to entire app. 
+const store = createStore(moviesApp, devToolsEnhancer()); 
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
     return (
       // Bootstrap Container for styling
-      <Container>
-        <MainView />
-      </Container>
+      // ** <Provider /> added for Redux use
+      <Provider store={store}> 
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
