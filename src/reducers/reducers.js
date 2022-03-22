b4ad0a38298fa0c857a08ccc6367424f67435577
jbettmann <jordan.bettmann@gmail.com>
2022-03-22 16:03:17 -0600

    added SET_USER to reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import { SET_FILTER, SET_MOVIES } from "../actions/actions";
+import { SET_FILTER, SET_MOVIES, SET_USER } from "../actions/actions";
 
 
 // if fucntions are "concerned" by the action, it will change the state
@@ -25,10 +25,20 @@ function movies(state = [], action) {
   }
 }
 
+function user(state = null, action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
 //  a combinded reducer (reducer made out of other reducers)
 const moviesApp = combineReducers({
     visibilityFilter,
-    movies
+    movies,
+    user
   });
 
 
