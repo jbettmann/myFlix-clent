c46e56ce331e83d8db8681a7e4c5248bdc4b04ec
jbettmann <jordan.bettmann@gmail.com>
2022-03-25 13:32:04 -0600

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,13 +3,9 @@
 export const SET_MOVIES = 'SET_MOVIES'; // initializes the movies list with movies
 export const SET_FILTER = 'SET_FILTER'; // sets filter to filter movies list
 export const SET_USER = 'SET_USER';
-<<<<<<< HEAD
-export const VALIDATE_INPUT = 'VALIDATE_INPUT';
-=======
 export const UPDATE_USER = 'UPDATE_USER';
 export const ADD_FAVORITE = 'ADD_FAVORITE';
 export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
->>>>>>> main
 
 // think of funcitons as event constructors. (value) can be name whatever you want. 
 export function setMovies(value) {
@@ -33,17 +29,6 @@ export function setUser(user) {
   }; 
 }
 
-<<<<<<< HEAD
-export function validateInput(
-  value = { Username: '', Email: '', Password: '', Birthday: '' },
-  field = null
-) {
-  return {
-    type: VALIDATE_INPUT,
-    value,
-    field,
-  };
-=======
 
 export function updateUser(user) {
   return {
@@ -64,5 +49,4 @@ export function removeFavorite(movie) {
     type: REMOVE_FAVORITE,
     movie
   }
->>>>>>> main
 }
