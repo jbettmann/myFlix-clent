094209f6f64f5e4fe04779735f934e41970e1c7f
jbettmann <jordan.bettmann@gmail.com>
2022-03-28 13:50:57 -0600

    rendered and displayed directors

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -7,7 +7,8 @@ export const DirectorView = ({directorName, onBackClick, movies}) => {
 
         // const { directorName, onBackClick, movies } = props;
 
-        const [d, setDirector] = useState({})
+        const [director, setDirector] = useState([])
+        
         useEffect(() => {
            let token = localStorage.getItem('token');
            console.log('directorName', directorName)
@@ -15,42 +16,42 @@ export const DirectorView = ({directorName, onBackClick, movies}) => {
              axios.get(`https://jordansmyflix.herokuapp.com/movies/directors/${directorName}`, {
                headers: {authorization: `Bearer ${token}`}
              }).then(response => {
-               setDirector({
-                 Name: response.data[0].Name,
-                 Birth: response.data[0].Birth,
-                 Death: response.data[0].Death,
-                 Bio: response.data[0].Bio
-               })
-               console.log(response.data)
+               setDirector(response.data)
              }).catch(error => {
                console.log('error:', error)
              })
            }
         }, [])
 
-        return (
-            <Container >
-              <Card id='movie-card'>
-                  <Card.Body>
-                      <Card.Title>{d.Name}</Card.Title>
-                      <Card.Text>
-                          <span className="label">Bio: </span>
-                          <span className="value">{d.Bio}</span>
-                      </Card.Text>
-                      <Card.Text>
-                          <span className="label">Birth: </span>
-                          <span className="value">{d.Birth}</span>
-                      </Card.Text>
-                      <Card.Text>
-                          <span className="label">Death: </span>
-                          <span className="value">{d.Death}</span>
-                      </Card.Text>
+      console.log(movies);
 
-                      <Button variant="light" onClick={() => { onBackClick(); }}>Back</Button>
-                  </Card.Body>
+        return (
+            <Container > 
+              <Card id='movie-card'>
+               {director.map((d, i) => {
+                 if (d.Name === directorName) {
+                  return (
+                    <Card.Body key={i}>   
+                        <Card.Title>{d.Name}</Card.Title>
+                        <Card.Text>
+                            <span className="label">Bio: </span>
+                            <span className="value">{d.Bio}</span>
+                        </Card.Text>
+                        <Card.Text>
+                            <span className="label">Birth: </span>
+                            <span className="value">{d.Birth}</span>
+                        </Card.Text>
+                        <Card.Text>
+                            <span className="label">Death: </span>
+                            <span className="value">{d.Death}</span>
+                        </Card.Text>  
+                        <Button variant="light" onClick={() => { onBackClick(); }}>Back</Button>  
+                    </Card.Body>
+                  )}
+               })}
               </Card>
               <Row className="favorite-movie">
-                {/* {movies.map(movie => (
+                {movies.map(movie => (
                   <Card id="movie-card " className="card-content mt-1" key={movie._id} >
                     <Link to={`/movies/${movie._id}`}> 
                       <Card.Img
@@ -60,7 +61,7 @@ export const DirectorView = ({directorName, onBackClick, movies}) => {
                         crossOrigin="true" />
                     </Link>
                   </Card>
-                ))} */}
+                ))}
               </Row>
             </Container>
         );
